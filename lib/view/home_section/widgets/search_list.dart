03c2d48573638b60930a739f7062af0d21d24e5f
AzharKV <mohammedazharkv@gmail.com>
2022-07-04 13:23:43 +0530

    bug fix: widget obx wrapping missed

--- lib/view/home_section/widgets/search_list.dart
+++ lib/view/home_section/widgets/search_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:super_ops/const/my_colors.dart';
 import 'package:super_ops/controllers/author_controller.dart';
 import 'package:super_ops/view/home_section/widgets/author_list.dart';
@@ -15,27 +16,30 @@ class SearchList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Search Results",
-                  style: TextStyle(color: MyColors.lightBlack, fontSize: 16.0)),
-              Text("${authorController.searchedAuthorsList.length} founds",
-                  style: const TextStyle(color: Colors.red, fontSize: 16.0)),
-            ],
-          ),
-          sizedBoxH8,
-          AuthorList(
-            authorController: authorController,
-            totalCount: authorController.searchedAuthorsList.length,
-            listData: authorController.searchedAuthorsList,
-            paginationEnable: false,
-            isSearchList: true,
-          ),
-        ],
+      child: Obx(
+        () => Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Search Results",
+                    style:
+                        TextStyle(color: MyColors.lightBlack, fontSize: 16.0)),
+                Text("${authorController.searchedAuthorsList.length} founds",
+                    style: const TextStyle(color: Colors.red, fontSize: 16.0)),
+              ],
+            ),
+            sizedBoxH8,
+            AuthorList(
+              authorController: authorController,
+              totalCount: authorController.searchedAuthorsList.length,
+              listData: authorController.searchedAuthorsList,
+              paginationEnable: false,
+              isSearchList: true,
+            ),
+          ],
+        ),
       ),
     );
   }
